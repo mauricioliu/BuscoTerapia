87fbd9ea7c24ab32da0d880c86c24fb3af5595cc
Mauricio Liu <liu.mauricio@gmail.com>
2012-01-31 02:40:33 -0400

    email aun...

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.1.3'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
-# gem 'mysql2'
+gem 'mysql2'
 gem 'pg'
 
 # Gems used only for assets and not required
@@ -28,6 +28,7 @@ gem "carrierwave"
 
 gem "will_paginate"
 gem "net-dns"
+gem "tlsmail"
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environment.rb
+++ config/environment.rb
@@ -1,23 +1,31 @@
+require 'tlsmail'
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
 
 BuscoTerapia::Application.initialize!
 
 BuscoTerapia::Application.configure do
   config.gem 'bcrypt-ruby', :lib => 'bcrypt'
-  config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-    :address              => "smtp.mailgun.org",
-    :port                 => 25,
-    :domain               => 'www.buscoterapia.com',
-    :user_name            => 'no-responder@buscoterapia.mailgun.org',
-    :password             => 'abc.123',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  
-    }
   end
+
+Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.raise_delivery_errors = true
+
+
+ActionMailer::Base.smtp_settings = {
+  :enable_starttls_auto => true,  
+  :address => 'smtp.mailgun.org',
+  :port => 25,
+  :domain => 'www.buscoterapia.com',
+  :user_name => 'no-responder@buscoterapia.mailgun.org',
+  :password => 'abc.123',
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+} 
