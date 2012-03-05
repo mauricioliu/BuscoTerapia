abefc30a3d445e64dcadeb01e93ef752ef3a2ba0
Mauricio Liu <liu.mauricio@gmail.com>
2012-03-05 00:25:18 -0330

    trying to fix email timeout in PROD

--- app/mailers/terapeuta_mailer.rb
+++ app/mailers/terapeuta_mailer.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class TerapeutaMailer < ActionMailer::Base
-  default from: "do-not-reply@buscoterapia.com"
+  default from: "no-reply@buscoterapia.cl"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
