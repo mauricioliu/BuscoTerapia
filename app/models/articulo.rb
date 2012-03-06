7959a77b639bb063123077d1fef3987accd3f371
Mauricio Liu <liu.mauricio@gmail.com>
2012-03-06 01:45:16 -0330

    adding validations to terapia and articulos

--- app/models/articulo.rb
+++ app/models/articulo.rb
@@ -1,6 +1,8 @@
 class Articulo < ActiveRecord::Base
   mount_uploader :imagen_url, ImageUploader
   
+  validates_presence_of :titulo, :autor, :especialidad, :contenido, :image_url
+  
   TIPO_DOCUMENTOS = [
     [ "articulo",   "articulo" ],
     [ "noticia",    "noticia" ]
--- app/models/terapia.rb
+++ app/models/terapia.rb
@@ -1,3 +1,5 @@
 class Terapia < ActiveRecord::Base
   mount_uploader :imagen_url, ImageUploader
+  
+  validates_presence_of :imagen_url, :nombre_corto, :descripcion_corta, :descripcion, :categoria, :caracteristica
 end
